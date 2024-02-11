563ed18a06327f19f4726421011c6e2a743d527c
Anoop Sivasankaran <anoop.sivasankaran@news.com.au>
2024-02-11 16:02:18 +1100

    math

--- src/Results.js
+++ src/Results.js
@@ -1,34 +1,32 @@
 import { Badge } from "./Badge";
 import { getAll } from "./storage";
+import moment from 'moment';
+
 import './Results.scss';
 
-function dateDiff(time) {
-    var datediff = (new Date().getTime())- time; 
-    const days = Math.floor(datediff / (24*60*60*1000));
-    if(days === 0) {
-        return 'Today';
-    }
-    if(days === 1) {
-        return 'Yesterday'
-    }
-    return `${days} days ago`;
+function dateDiff(startDate, endDate) {
+
+    var duration = moment.duration(moment(endDate).diff(moment(startDate)));
+    var hours = Math.floor(duration.asSeconds());
+    return `(${hours} sec)`; 
 }
 
 export default function Results() {
     const results  = getAll();
     const keys = Object.keys(results).sort((a, b) => b - a);
-
+    
     return (
         <div className="results">
             {
                 keys.map((key) => {
-                    const {badge, probType, difficulty, endDate} = results[key];
+                    const {badge, probType, difficulty, endDate, startDate} = results[key];
                     return (
                         <div className="results-item" key={endDate}>
                             <Badge type={badge} />
                             <div>{probType}</div>
                             <div className={`difficulty ${difficulty}`}>{difficulty}</div>
-                            <div className="day">{dateDiff(endDate)}</div>
+                            <div className="day">{moment(endDate).fromNow()}</div>
+                            <div className="day">{dateDiff(startDate, endDate)}</div>
                         </div>
                     );
                 })
